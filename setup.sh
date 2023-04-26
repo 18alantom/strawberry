17c2b80e4c961a01b2ec301f0c8ae1fa0e83c599
18alantom <2.alan.tom@gmail.com>
2023-04-26 20:32:48 +0530

    add setup script

---
+++ setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+if [ -d "sb-starter" ]; then
+  echo "Folder sb-starter already exists. Exiting."
+  exit 1
+fi
+
+mkdir sb-starter
+cd sb-starter
+
+curl -so sb.min.js https://raw.githubusercontent.com/18alantom/strawberry/main/dist/sb.min.js
+index_html=$(cat <<EOF
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Strawberry App</title>
+    <script src="sb.min.js"></script>
+  </head>
+
+  <style>
+    body {
+      height: 100vh;
+      width: 100vw;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
+
+  <!-- Template Definitions -->
+  <template name="sb-block">
+    <div
+      style="
+        padding: 1rem;
+        background-color: hsla(43, 58%, 92%, 1);
+        border: 2px solid hsla(17, 48%, 16%, 1);
+        box-shadow: -4px 4px 0px rgba(0, 0, 0, 0.25);
+      "
+    ></div>
+  </template>
+  <template name="sb-h1">
+    <h1
+      style="
+        font-family: sans-serif;
+        font-weight: bold;
+        width: fit-content;
+        color: hsla(6, 64%, 59%, 1);
+      "
+    >
+      <slot />
+    </h1>
+  </template>
+
+  <!-- Initialize Strawberry -->
+  <script>
+    const data = sb.init();
+  </script>
+
+  <!-- HTML Body -->
+  <body>
+    <!-- Use defined components -->
+    <sb-block>      
+      <sb-h1 sb-mark="message">Hello, World!</sb-h1>
+    </sb-block>
+
+    <!-- Update Component -->
+    <script>
+      data.message = 'Hello, Strawberry! 🍓';
+    </script>
+  </body>
+</html>
+EOF
+)
+
+echo "$index_html" > index.html
+cd ..
+echo "sb-starter setup completed
+open index.html and start coding :)"
